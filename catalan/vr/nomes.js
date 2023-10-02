98217d5c4e091d2f08193a7b8401066bceca759c
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2023-10-02 09:24:16 -0300

    nomes

---
+++ catalan/vr/nomes.js
@@ -0,0 +1,35 @@
+function mfaces() {
+	var x = document.getElementById("faces")
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelector('#fc')
+	if (x.checked == true) { 
+	  entityEl.setAttribute('visible','true');
+	}
+	else {
+	  entityEl.setAttribute('visible','false');
+	}
+}
+
+function marestas() {
+	var x = document.getElementById("arestas")
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelector('#ar')
+	if (x.checked == true) { 
+	  entityEl.setAttribute('visible','true');
+	}
+	else {
+	  entityEl.setAttribute('visible','false');
+	}
+}
+
+function mvertices() {
+	var x = document.getElementById("vertices")
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelector('#esfera')
+	if (x.checked == true) { 
+	  entityEl.setAttribute('visible','true');
+	}
+	else {
+	  entityEl.setAttribute('visible','false');
+	}
+}
