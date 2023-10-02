c1546a93600cd29f7d7a4f1c1b7bd17f473aa70b
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2023-10-02 09:41:24 -0300

    nomes

---
+++ platonic/vr/nomes.js
@@ -0,0 +1,23 @@
+function mfaces() {
+	var x = document.getElementById("faces")
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelector('#fc')
+	if (x.checked == true) { 
+	  entityEl.setAttribute('visible','true');
+	}
+	else {
+	  entityEl.setAttribute('visible','false');
+	}
+}
+
+function marestas() {
+	var x = document.getElementById("ar")
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelector('#arestas')
+	if (x.checked == true) { 
+	  entityEl.setAttribute('visible','true');
+	}
+	else {
+	  entityEl.setAttribute('visible','false');
+	}
+}
