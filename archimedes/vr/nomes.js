806e5e0db9193f2a56f854db7f802fefc3980c74
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2023-10-29 20:21:18 -0300

    update

--- archimedes/vr/nomes.js
+++ archimedes/vr/nomes.js
@@ -21,3 +21,37 @@ function marestas() {
 	  entityEl.setAttribute('visible','false');
 	}
 }
+
+function mfacesRV() {
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelectorAll('.faces')
+	var x = document.getElementById("face")
+	if (x.checked == true) { 
+	  for (let i = 0; i < entityEl.length; i++) {
+		entityEl[i].setAttribute('visible','true');
+	  }
+	  x.checked = false;
+	} else {
+	  for (let i = 0; i < entityEl.length; i++) {
+		entityEl[i].setAttribute('visible','false');
+	  }
+	  x.checked = true;
+	}
+}
+
+function marestasRV() {
+	var sceneEl = document.querySelector('a-scene')
+	var entityEl = sceneEl.querySelectorAll('.arestas')
+	var x = document.getElementById("ar")
+	if (x.checked == true) { 
+	  for (let i = 0; i < entityEl.length; i++) {
+		entityEl[i].setAttribute('visible','true');
+	  }
+	  x.checked = false;
+	} else {
+	  for (let i = 0; i < entityEl.length; i++) {
+		entityEl[i].setAttribute('visible','false');
+	  }
+	  x.checked = true;
+	}
+}
