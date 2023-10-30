cc97a399615de8fd8247fef22b2ba0c9d73f9db0
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2023-10-29 21:41:40 -0300

    update

--- archimedes/vr/nomes.js
+++ archimedes/vr/nomes.js
@@ -1,5 +1,5 @@
 function mfaces() {
-	var x = document.getElementById("faces")
+	var x = document.getElementById('faces')
 	var sceneEl = document.querySelector('a-scene')
 	var entityEl = sceneEl.querySelector('#fc')
 	if (x.checked == true) { 
@@ -11,7 +11,7 @@ function mfaces() {
 }
 
 function marestas() {
-	var x = document.getElementById("ar")
+	var x = document.getElementById('ar')
 	var sceneEl = document.querySelector('a-scene')
 	var entityEl = sceneEl.querySelector('#arestas')
 	if (x.checked == true) { 
@@ -23,52 +23,58 @@ function marestas() {
 }
 
 function mvertices() {
-	var x = document.getElementById("vertices")
+	var x = document.getElementById('vertices')
 	var sceneEl = document.querySelector('a-scene')
 	var entityEl = sceneEl.querySelector('#esfera')
 	var entityEl0 = sceneEl.querySelector('#esferaG')
 	if (x.checked == true) { 
 	  entityEl.setAttribute('visible','true');
 	  entityEl0.setAttribute('visible','true');
-	  document.getElementById("vertices").checked = false;
+	  document.getElementById('vertices').checked = false;
+	  x.checked = false;
 	}
 	else {
 	  entityEl.setAttribute('visible','false');
 	  entityEl0.setAttribute('visible','false');
-	  document.getElementById("vertices").checked = true;
+	  document.getElementById('vertices').checked = true;
+	  x.checked = true;
 	}
 }
 
 function mfacesRV() {
 	var sceneEl = document.querySelector('a-scene')
 	var entityEl = sceneEl.querySelectorAll('.faces')
-	var x = document.getElementById("face")
+	let x = document.getElementById('face')
 	if (x.checked == true) { 
 	  for (let i = 0; i < entityEl.length; i++) {
 		entityEl[i].setAttribute('visible','true');
 	  }
-	  document.getElementById("face").checked = false;
+	  document.getElementById('face').checked = false;
+	  x.checked = false;
 	} else {
 	  for (let i = 0; i < entityEl.length; i++) {
 		entityEl[i].setAttribute('visible','false');
 	  }
-	  document.getElementById("face").checked = true;
+	  document.getElementById('face').checked = true;
+      x.checked = true;
 	}
 }
 
 function marestasRV() {
 	var sceneEl = document.querySelector('a-scene')
 	var entityEl = sceneEl.querySelectorAll('.arestas')
-	var x = document.getElementById("ar")
+	var x = document.getElementById('ar')
 	if (x.checked == true) { 
 	  for (let i = 0; i < entityEl.length; i++) {
 		entityEl[i].setAttribute('visible','true');
 	  }
-	  document.getElementById("ar").checked = false;
+	  document.getElementById('ar').checked = false;
+      x.checked = false;
 	} else {
 	  for (let i = 0; i < entityEl.length; i++) {
 		entityEl[i].setAttribute('visible','false');
 	  }
-	  document.getElementById("ar").checked = true;
+	  document.getElementById('ar').checked = true;
+      x.checked = true;
 	}
 }
